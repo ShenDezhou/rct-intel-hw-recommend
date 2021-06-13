be2f9d2e75fb689c9de0655c32c64711acdd1dfd
ShenDezhou <tsinghua9boy@sina.com>
2021-06-13 19:45:29 +0800

    1.loss change from binary_crossentropy to kl_div.

--- pytorch/baseline.py
+++ pytorch/baseline.py
@@ -456,7 +456,7 @@ if __name__ == "__main__":
             betas=(0.9, 0.999),
             weight_decay=1e-8,
             correct_bias=False)
-        model.compile(optimizer=optimizer, loss=torch.nn.functional.kl_div, metrics=[torch.nn.functional.kl_div, "auc"])
+        model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['binary_crossentropy', "auc"])
 
         history = model.fit(train_model_input, train[target].values, batch_size=512, epochs=5, verbose=1,
                             validation_split=0.2)
