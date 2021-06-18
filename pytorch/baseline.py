a463396ad2cf9ff4e20c7b0ff39cf9fb69ae4a59
ShenDezhou <tsinghua9boy@sina.com>
2021-06-18 09:40:14 +0800

    1. recommendation

--- pytorch/baseline.py
+++ pytorch/baseline.py
@@ -290,7 +290,7 @@ class MyDeepFM(MyBaseModel):
 class MyAFTDeepFM(MyBaseModel):
     def __init__(self,
                  linear_feature_columns, dnn_feature_columns, use_fm=True,
-                 dnn_hidden_units=(256, 128),
+                 dnn_hidden_units=(64, 16),
                  l2_reg_linear=0.00001, l2_reg_embedding=0.00001, l2_reg_dnn=0, init_std=0.0001, seed=1024,
                  dnn_dropout=0,
                  dnn_activation='relu', dnn_use_bn=False, task='binary', device='cpu', gpus=None):
@@ -346,7 +346,7 @@ class MyAFTDeepFM(MyBaseModel):
         return module(
             max_seqlen=5,
             dim=4, # Embedding 4
-            hidden_dim=192,
+            hidden_dim=384,
             device=device
         )
 
